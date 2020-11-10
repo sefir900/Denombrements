e754744678d0480680a2c654415b1ff8c9b6471d
sefir900 <74006158+sefir900@users.noreply.github.com>
2020-11-10 12:01:32 +0100

    Revert "changement if switch"

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -8,33 +8,34 @@ namespace Denombrements
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-            string choix;
-            choix = "1";
+            int c = 1;
+            while (c != 0)
             {
                 Console.WriteLine("Permutation ...................... 1");
                 Console.WriteLine("Arrangement ...................... 2");
                 Console.WriteLine("Combinaison ...................... 3");
                 Console.WriteLine("Quitter .......................... 0");
                 Console.Write("Choix :                            ");
-                int.Parse(Console.ReadLine());
+                c = int.Parse(Console.ReadLine());
 
-                switch (choix)
+                if (c == 0) { Environment.Exit(0); }
+
+                if (c == 1)
                 {
-                    case "1":
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int n1 = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                                // calcul de r
-                        long r1 = 1;
-                        for (int k = 1; k <= n1; k++)
-                            r1 *= k;
-                        Console.WriteLine(n1 + "! = " + r1);
-                        break;
-
-                    case "2":
+                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                    int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                                                           // calcul de r
+                    long r = 1;
+                    for (int k = 1; k <= n; k++)
+                        r *= k;
+                    Console.WriteLine(n + "! = " + r);
+                }
+                else
+                {
+                    if (c == 2)
+                    {
                         Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
                         int t = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
@@ -45,12 +46,25 @@ namespace Denombrements
                             r *= k;
                         //Console.WriteLine("résultat = " + (r1 / r2));
                         Console.WriteLine("A(" + t + "/" + n + ") = " + r);
-
-                        break;
-
-                    case "0":
-                        Environment.Exit(0);
-                        break;
+                    }
+                    else
+                    {
+                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
+                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
+                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        // calcul de r1
+                        long r1 = 1;
+                        for (int k = (t - n + 1); k <= t; k++)
+                            r1 *= k;
+                        // calcul de r2
+                        long r2 = 1;
+                        for (int k = 1; k <= n; k++)
+                            r2 *= k;
+                        // calcul de r3
+                        //Console.WriteLine("résultat = " + (r1 / r2));
+                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
+                    }
                 }
             }
             Console.ReadLine();
